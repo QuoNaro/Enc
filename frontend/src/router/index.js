0e0b0f47cf5a3138934642184977a4c0993e3837
quonaro <quonaro@mail.ru>
2025-02-05 17:12:00 +0700

    edit routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import UserRegister from '../components/UserRegister.vue';
-import UserLogin from '../components/UserLogin.vue';
 import UserAuth from '@/components/UserAuth.vue';
 
 
@@ -9,23 +7,12 @@ Vue.use(Router);
 
 const routes = [
   {
-    path: '/register',
-    name: 'UserRegister',
-    component: UserRegister
-  },
-  {
-    path: '/login',
-    name: 'UserLogin',
-    component: UserLogin
-  },
-
-  {
     path: '/auth',
     name: 'UserAuth',
     component: UserAuth,
     children: [
-      { path: '#signin', component: UserLogin },
-      { path: '#signup', component: UserRegister }
+      { path: '#signin'},
+      { path: '#signup'}
     ]
   },
 ];
